7371d6e65d10dab1e4bc08d2b8eaa8166af29158
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2024-10-05 23:13:09 +0300

    feat(signup): add jwt token in response

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,13 @@
-const userService = require("../services/userService");
+const { sendSuccess } = require('../../utils');
+const userService = require('../services/userService');
 
 async function createUser(req, res, next) {
     try {
         const details = req.body;
         const data = await userService.createUser(details);
-        res.status(201).json({ data: data }).end();
+        sendSuccess(req, res, { token: data }, 'user created', 201);
     } catch (err) {
-        res.status(400).json({ message: err.message }).end();
+        next();
     }
 }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,15 +1,29 @@
-const { userModel } = require("../models/userModel");
-const { v4: uuidv4 } = require("uuid");
-const bcrypt = require("bcrypt");
-const { ACCESS } = require("../config");
+const { userModel } = require('../models/userModel');
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
+const { ACCESS } = require('../config');
+const jwt = require('jsonwebtoken');
 
 async function createUser(details) {
     details.userId = uuidv4();
-    const hashedPass = await bcrypt.hash(details.password, +ACCESS.hashSaltRounds);
+    const hashedPass = await bcrypt.hash(
+        details.password,
+        +ACCESS.hashSaltRounds,
+    );
     details.password = hashedPass;
     const newUser = new userModel(details);
-    const result = await newUser.save();
-    return result;
+    await newUser.save();
+    const token = jwt.sign(
+        {
+            id: newUser.userId,
+            username: newUser.username,
+            email: newUser.email,
+            // Add any other details you want in the token
+        },
+        process.env.JWT_SECRET, // The secret key
+        { expiresIn: '1h' }, // Token expiration time
+    );
+    return token;
 }
 
 module.exports = { createUser };
